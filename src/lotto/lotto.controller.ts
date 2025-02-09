82b4b06c036273204a30be539abfb2a038a79275
npbjr <normanbanzonjr@gmail.com>
2025-02-09 15:01:57 +0800

    include search on findAll lotto Results

--- src/lotto/lotto.controller.ts
+++ src/lotto/lotto.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Body, Param, Query } from '@nestjs/common';
 import { LottoService } from './lotto.service';
 
 @Controller('lotto')
 export class LottoController {
     constructor(private readonly lottoService:LottoService){}
     @Get('result')
-    async findAll(): Promise<any> {
-        return await this.lottoService.findAll()
+    async findAll(@Query('searchText') searchText: string): Promise<any> {
+        console.error("any", searchText)
+        return await this.lottoService.findAll(searchText)
     }
 }
--- src/lotto/lotto.service.ts
+++ src/lotto/lotto.service.ts
@@ -1,31 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { LottoResult } from './lotto.results.schema';
 import { Repository } from 'typeorm';
+import { LottoResult } from './lotto.results.schema';
 import { DrawSchedule } from './lotto.drawschedule.schema';
+
 @Injectable()
 export class LottoService {
-    constructor(@InjectRepository(
-        LottoResult) private readonly lottoResult: Repository<LottoResult>,
-        @InjectRepository(DrawSchedule) private readonly drawSchedule: Repository<DrawSchedule>
-    ){}
+  constructor(
+    @InjectRepository(LottoResult) private readonly lottoResult: Repository<LottoResult>,
+    @InjectRepository(DrawSchedule) private readonly drawSchedule: Repository<DrawSchedule>,
+  ) {}
 
-    async findAll():Promise<any>{
-    
-
-
+  async findAll(searchText?: string): Promise<any> {
     const lottoResults = await this.lottoResult.find({
-        relations: ['lottoType', 'schedule'], 
-      });
-  
-      return lottoResults.map(result => ({
-        id: result.id,
-        numbers: result.numbers,
-        drawdate: result.drawdate,
-        lottoType: result.lottoType?.name, 
-        schedule: result.schedule?.time,
-      }));
-        }
-    
+      relations: ['lottoType', 'schedule'],
+    });
 
+    const filteredResults = searchText
+    ? lottoResults.filter(result => {
+        const searchNumbers = searchText.split(' ').map(num => num.trim());
+
+      
+        const resultNumbers = result.numbers.split(',').map(num => num.trim());
+
+       
+        return searchNumbers.every(searchNum => resultNumbers.includes(searchNum));
+      })
+    : lottoResults;
+
+
+    return filteredResults.map(result => ({
+      id: result.id,
+      numbers: result.numbers,
+      drawdate: result.drawdate,
+      lottoType: result.lottoType?.name,
+      schedule: result.schedule?.time,
+    }));
+  }
 }
